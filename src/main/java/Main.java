f40c9ea836a68510357da1fdbb60445a4621f4d4
MarinaLuchitska <lmarina3891@gmail.com>
2026-02-09 22:31:50 +0200

    #1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,43 @@
-package org.example;
+import models.Word;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+
+
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        StandardServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+                .configure("hibernate.cfg.xml")
+                .build();
+
+        Metadata metadata = new MetadataSources(serviceRegistry)
+                .addAnnotatedClass(Word.class)
+                .getMetadataBuilder()
+                .build();
+
+        SessionFactory sessionFactory = metadata.getSessionFactoryBuilder().build();
+        Session session = sessionFactory.openSession();
+        session.beginTransaction();
+
+        session.save(new Word("kokos"));
+        session.save(new Word("abricos"));
+        session.save(new Word("lalala"));
+
+
+
+        List<Word> list = session.createNativeQuery("select * from  word", Word.class).list();
+        System.out.println(list);
+
+        session.getTransaction().commit();
+        session.close();
+        sessionFactory.close();
+
+
     }
 }
--- src/main/java/models/Word.java
+++ src/main/java/models/Word.java
@@ -1,10 +1,42 @@
 package models;
 
-
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
-
 @Entity
-public class User implements Serializable {
+public class Word implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String value;
 
+    public Word(String value) {
+        this.value = value;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Word{" +
+                "id=" + id +
+                ", value='" + value + '\'' +
+                '}';
+    }
 }
